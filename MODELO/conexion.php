9efa6b97caeafb261181b15c16d233fcc4290218
Leslie <126528844+LesliePacheco91@users.noreply.github.com>
2025-05-26 17:50:07 -0600

    Update conexion.php

--- MODELO/conexion.php
+++ MODELO/conexion.php
@@ -6,10 +6,7 @@
 class Conexion{
 
 	static public function conectar(){
-	$con = new PDO("mysql:host=utdelmayab.edu.mx;dbname=utdelmay_intrautm","utdelmay_intraut","MySQL1ntr4utm",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
-	#$con = new PDO("mysql:host=localhost;dbname=utdelmay_intrautm","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
+	$con = new PDO("mysql:host=localhost;dbname=utdelmay","admin","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 	return $con;
 	}
 }
